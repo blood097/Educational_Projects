31f00c8cc4a8778a972c622de8c7ebcdbce7da26
Evgenii Kirichek <107462781+blood097@users.noreply.github.com>
2023-01-24 17:48:46 +0300

    Update Test_SQL2.sql

--- Тестовые задания/Junior_Analyst_in_Syber_Interactive/Test_SQL2.sql
+++ Тестовые задания/Junior_Analyst_in_Syber_Interactive/Test_SQL2.sql
@@ -1,15 +1,15 @@
 CREATE TEMP TABLE t1 AS
 SELECT issue_key, 
-	   status,
-	   DATETIME(started_at / 1000, 'unixepoch') AS started_datetime
+	status,
+	DATETIME(started_at / 1000, 'unixepoch') AS started_datetime
 FROM history
 WHERE status != "Closed" AND status != "Resolved";
 
 CREATE TEMP TABLE variable (name TEXT PRIMARY KEY, value TEXT);
 INSERT OR REPLACE INTO variable VALUES ('Дата для поиска открытых задач', DATETIME()); -- Вместо DATETIME() тут можно написать любую дату для которой будут искать задачи
-										                                               -- сейчас запрос выдает все до текущей даты и времени
+										       -- сейчас запрос выдает все до текущей даты и времени
 SELECT DISTINCT(issue_key),
-	   LAST_VALUE(status) OVER (PARTITION BY issue_key) AS last_status,
-	   MAX(started_datetime) OVER (PARTITION BY issue_key) AS last_time
+	LAST_VALUE(status) OVER (PARTITION BY issue_key) AS last_status,
+	MAX(started_datetime) OVER (PARTITION BY issue_key) AS last_time
 FROM t1
 WHERE started_datetime <= (SELECT value FROM variable)
