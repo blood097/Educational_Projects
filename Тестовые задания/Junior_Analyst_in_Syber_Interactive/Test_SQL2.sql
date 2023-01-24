0ada8bea86432f64522e24509e7b8cc4905d166b
Evgenii Kirichek <107462781+blood097@users.noreply.github.com>
2023-01-24 17:48:10 +0300

    Update Test_SQL2.sql

--- Тестовые задания/Junior_Analyst_in_Syber_Interactive/Test_SQL2.sql
+++ Тестовые задания/Junior_Analyst_in_Syber_Interactive/Test_SQL2.sql
@@ -7,7 +7,7 @@ WHERE status != "Closed" AND status != "Resolved";
 
 CREATE TEMP TABLE variable (name TEXT PRIMARY KEY, value TEXT);
 INSERT OR REPLACE INTO variable VALUES ('Дата для поиска открытых задач', DATETIME()); -- Вместо DATETIME() тут можно написать любую дату для которой будут искать задачи
-																					   -- сейчас запрос выдает все до текущей даты и времени
+										                                               -- сейчас запрос выдает все до текущей даты и времени
 SELECT DISTINCT(issue_key),
 	   LAST_VALUE(status) OVER (PARTITION BY issue_key) AS last_status,
 	   MAX(started_datetime) OVER (PARTITION BY issue_key) AS last_time
