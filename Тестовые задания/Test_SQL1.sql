09988a6d974cc8ca3b1d7eb24a79ad4d080be99a
Evgenii Kirichek <107462781+blood097@users.noreply.github.com>
2023-01-24 15:03:50 +0300

    Update Test_SQL1.sql

--- Тестовые задания/Test_SQL1.sql
+++ Тестовые задания/Test_SQL1.sql
@@ -1,5 +1,5 @@
 SELECT issue_type,
-	   round(AVG(minutes_in_status/60),2) AS avg_time_in_hours
+       round(AVG(minutes_in_status/60),2) AS avg_time_in_hours
 FROM (SELECT *, SUBSTRING(issue_key,1,1) AS issue_type
       FROM history)
 WHERE status == "Open"
